f67aabcdf2a89048fc114d8317c954b6fbea8c81
Nilesh Chakraborty <nilesh@nileshc.com>
2015-09-24 14:54:31 +0200

    Set taConfidence to spotting confidence scores from StanfordNER

--- src/main/scala/org/elinker/core/api/process/EntityLinker.scala
+++ src/main/scala/org/elinker/core/api/process/EntityLinker.scala
@@ -6,20 +6,16 @@ import java.util
 import akka.actor.SupervisorStrategy.Stop
 import akka.actor.{OneForOneStrategy, Actor}
 import akka.event.Logging
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.hp.hpl.jena.vocabulary.RDF
-import edu.stanford.nlp.ie.crf.CRFClassifier
-import org.aksw.gerbil.transfer.nif.data.{SpanImpl, NamedEntity}
+import edu.stanford.nlp.ie.crf.{CRFCliqueTree, CRFClassifier}
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.util.CoreMap
 import org.aksw.gerbil.transfer.nif._
 import org.apache.solr.client.solrj.SolrQuery
 import org.apache.solr.client.solrj.impl.HttpSolrClient
 import org.apache.solr.client.solrj.util.ClientUtils
 import org.elinker.serialize.NIFConverter
-import spray.http.StatusCode
 import spray.http.StatusCodes._
-import spray.json.RootJsonFormat
 import spray.routing.RequestContext
-
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ListBuffer
 
@@ -35,7 +31,7 @@ object EntityLinker {
   case class GerbilDisambiguate(nif: String, language: String, dataset: String)
 }
 
-class EntityLinker(rc: RequestContext, nerClassifier: CRFClassifier[_]) extends Actor {
+class EntityLinker[T <: CoreMap](rc: RequestContext, nerClassifier: CRFClassifier[T]) extends Actor {
   import EntityLinker._
   import context._
 
@@ -55,14 +51,11 @@ class EntityLinker(rc: RequestContext, nerClassifier: CRFClassifier[_]) extends 
 
     val query = new SolrQuery()
     query.set("q", s"""label:"${e(mention)}" AND dataset:"$dataset" AND (language:"$language" OR language:"xx")""")
-    query.set("fl", "*,score")
     query.set("sort", "score desc, count desc")
     query.set("rows", 10)
 
     val response = solr.query("elinker", query)
     val results = response.getResults
-    val highlights = response.getHighlighting
-    val maxScore = results.getMaxScore
 
     if(results.isEmpty) {
       Nil
@@ -70,47 +63,65 @@ class EntityLinker(rc: RequestContext, nerClassifier: CRFClassifier[_]) extends 
       results.map {
         case document =>
           val resource = document.get("resource").asInstanceOf[String]
-          val relevance = document.get("score").asInstanceOf[Float].toDouble / maxScore
+          val relevance = 0.0
           (resource, relevance)
       }.take(maxLinks).toSeq
     }
   }
 
-  def getMentions(text: String, language: String): List[Result] = {
-    // Fetch entity mentions in text (only spotting)
-    val results = new ListBuffer[Result]()
-    val triples = nerClassifier.classifyToCharacterOffsets(text)
-    for(triple <- triples) {
-      val entityType = triple.first()
-      val begin = triple.second()
-      val end = triple.third()
-      val phrase = text.substring(begin, end)
-      results.add(Result(entityType, phrase, begin, end, None, None))
-    }
-//    println(results.mkString("\n"))
-    results.toList
+
+  def getMentions(text: String): Seq[Result] = {
+    // Fetch entity mentions in text (only spotting) along with confidence scores
+    val sentences = nerClassifier.classify(text).get(0)
+    val p = nerClassifier.documentToDataAndLabels(sentences)
+    val cliqueTree: CRFCliqueTree[String] = nerClassifier.getCliqueTree(p)
+    val entities = ListBuffer[(Int, Int, String, Double)]()
+
+    var currentBegin = 0
+    var currentEnd = 0
+    var tokensInCurrentEntity = 0
+    var currentClassLabel = "O"
+    var currentProbs = 0.0
+    var entityMention = ""
+
+    (for((doc, i) <- sentences.zipWithIndex;
+         mention = doc.get(classOf[CoreAnnotations.TextAnnotation]);
+         begin = doc.get(classOf[CoreAnnotations.CharacterOffsetBeginAnnotation]);
+         end = doc.get(classOf[CoreAnnotations.CharacterOffsetEndAnnotation]);
+         (classLabel, prob) = (for((classLabel, j) <- nerClassifier.classIndex.objectsList().zipWithIndex) yield (classLabel, cliqueTree.prob(i, j))).maxBy(_._2)
+    ) yield {
+        if (currentClassLabel != classLabel && currentClassLabel != "O") {
+          val result = Result(currentClassLabel, entityMention, currentBegin, currentEnd, None, Some(currentProbs / tokensInCurrentEntity))
+          currentBegin = 0
+          currentEnd = 0
+          tokensInCurrentEntity = 0
+          currentClassLabel = classLabel
+          entityMention = ""
+          currentProbs = 0.0
+          Seq(result)
+        } else {
+          if(classLabel != "O") {
+            if(tokensInCurrentEntity == 0) currentBegin = begin
+            tokensInCurrentEntity += 1
+            currentEnd = end
+            currentClassLabel = classLabel
+            entityMention = if(entityMention.isEmpty) mention else entityMention + " " + mention
+            currentProbs += prob
+          }
+          Nil
+        }
+      }).flatten
   }
 
-  def getEntities(text: String, language: String, dataset: String, linksPerMention: Int): List[Result] = {
+  def getEntities(text: String, language: String, dataset: String, linksPerMention: Int): Seq[Result] = {
     // Spot entities and link to given dataset
-    val results = new ListBuffer[Result]()
-    val triples = nerClassifier.classifyToCharacterOffsets(text)
-    for(triple <- triples) {
-      val entityType = triple.first()
-      val begin = triple.second()
-      val end = triple.third()
-      val phrase = text.substring(begin, end)
+    (for(result @ Result(entityType, phrase, begin, end, _, Some(score)) <- getMentions(text)) yield {
       val links = linkToKB(phrase, dataset, language, linksPerMention)
-
-      if(links.isEmpty) {
-        results.add(Result(entityType, phrase, begin, end, None, None))
-      } else {
-        for ((link, score) <- links) {
-          results.add(Result(entityType, phrase, begin, end, Some(link), Some(score)))
-        }
-      }
-    }
-    results.toList
+      if(links.isEmpty)
+        Seq(result)
+      else
+        for((link, _) <- links) yield result.copy(taIdentRef = Some(link))
+    }).flatten
   }
 
   def outputAnnotatedDocument(document: Document, annotations: util.List[Marking]): Unit = {
@@ -122,7 +133,7 @@ class EntityLinker(rc: RequestContext, nerClassifier: CRFClassifier[_]) extends 
 
   def receive = {
     case SpotEntities(text, language, outputFormat, prefix, classify) =>
-      val results = getMentions(text, language)
+      val results = getMentions(text)
 
       val nif = new NIFConverter(prefix)
       val contextModel = nif.createContext(text, 0, text.length)
@@ -130,11 +141,11 @@ class EntityLinker(rc: RequestContext, nerClassifier: CRFClassifier[_]) extends 
 
 
       results.foreach {
-        case Result(entityType, mention, begin, end, _, _) =>
+        case Result(entityType, mention, begin, end, _, Some(score)) =>
           val mentionModel = if(classify)
-            nif.createMentionWithType(entityType, mention, begin, end, contextRes)
+            nif.createMentionWithTypeAndScore(entityType, mention, begin, end, score, contextRes)
           else
-            nif.createMention(mention, begin, end, contextRes)
+            nif.createMentionWithScore(mention, begin, end, score, contextRes)
 
           // Merge the context and the mention.
           contextModel.add(mentionModel)
@@ -166,11 +177,11 @@ class EntityLinker(rc: RequestContext, nerClassifier: CRFClassifier[_]) extends 
                 nif.createLinkWithType(entityType, mention, begin, end, ref, contextRes)
               else
                 nif.createLink(mention, begin, end, ref, contextRes)
-            case _ =>
+            case (None, Some(score)) =>
               if(classify)
-                nif.createMentionWithType(entityType, mention, begin, end, contextRes)
+                nif.createMentionWithTypeAndScore(entityType, mention, begin, end, score, contextRes)
               else
-                nif.createMention(mention, begin, end, contextRes)
+                nif.createMentionWithScore(mention, begin, end, score, contextRes)
           }
 
           // Merge the context and the mention.
@@ -199,7 +210,7 @@ class EntityLinker(rc: RequestContext, nerClassifier: CRFClassifier[_]) extends 
           mention = text.substring(begin, end);
           ref = linkToKB(mention, dataset, language, 1)
           if ref.nonEmpty
-      ) yield {
+      ) {
         contextModel.add(nif.createLink(mention, begin, end, ref.head._1, contextRes))
       }
 
