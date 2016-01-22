1f43cb41738431ee428ce8946d59b09df5dcacdc
Nilesh Chakraborty <nilesh@nileshc.com>
2016-01-22 19:17:21 +0100

    Fix DB connection leak by moving ApplicationContext to singleton

--- src/main/scala/org/elinker/core/api/service/ApiService.scala
+++ src/main/scala/org/elinker/core/api/service/ApiService.scala
@@ -1,19 +1,18 @@
 package org.elinker.core.api.service
 
 import eu.freme.common.persistence.dao.DatasetSimpleDAO
-import org.elinker.core.api.auth.TokenAuthentication
 import org.elinker.core.api.scala.Config
 import org.elinker.core.api.java
 import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
-import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * Created by nilesh on 16/12/2014.
  */
 trait ApiService extends EntityApiService with DatasetApiService {
 
-  def getSpringContext: ApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")
+  import ApiService._
+  def getSpringContext: ApplicationContext = springContext
 
   override def getDatasetDAO: DatasetSimpleDAO = getSpringContext.getBean("datasetSimpleDAO").asInstanceOf[DatasetSimpleDAO]
   override def getConfig: Config = getSpringContext.getBean("config").asInstanceOf[java.Config].getScalaConfig
@@ -22,4 +21,8 @@ trait ApiService extends EntityApiService with DatasetApiService {
     pathPrefix("api") {
           entityRoute ~ datasetRoute
     }
+}
+
+object ApiService {
+  val springContext: ApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")
 }
