563316b56deec12d8de9a45630cea45e76feae1a
sandro.coelho@ice.ufjf.br <sandro.coelho@ice.ufjf.br>
2016-07-23 15:32:48 -0300

    Solr dump script improvement #114

--- script/freme-ner-dump.sh
+++ script/freme-ner-dump.sh
@@ -2,11 +2,14 @@
 
 readonly START=0
 readonly TOTAL=88000000
+readonly URL=http://rv2622.1blu.de/solrlive
 
 counter=0
 
+mkdir dump
+
 while [ $counter -le $TOTAL ]
 do
-   curl "http://rv2622.1blu.de/solrlive/elinker/select?q=*%3A*&start=$counter&rows=$(($counter + 1000))&wt=csv&indent=true" >> freme-ner.csv
-   counter=$(( $counter + 1000 ))
+   curl "${URL}/elinker/select?q=*%3A*&start=$counter&rows=$(($counter + 1000000))&wt=csv&indent=true" > dump/freme-ner-${counter}.csv
+   counter=$(( $counter + 1000000 ))
 done
